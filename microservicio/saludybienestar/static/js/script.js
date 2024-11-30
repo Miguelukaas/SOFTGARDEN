886628b0d989bd5146d70646e0a97decef401cfc
Miguel Mora <miguel.mora1629@alumnos.udg.mx>
2024-11-29 19:07:50 -0600

    Delete microservicio directory

--- microservicio/saludybienestar/static/js/script.js
+++
@@ -1,6 +0,0 @@
-const registerLink = document.querySelector('.register-link');
-const registerForm = document.querySelector('.register-form');
-
-registerLink.addEventListener('click', () => {
-    registerForm.classList.toggle('hidden');
-});
